1a7ef895ded04877cc3da0a52c42dbd9d377c857
이건우 <lgw9898@naver.com>
2024-10-03 22:11:00 +0900

    chore(spm): SharedFoundation 패키지 스캐폴딩

---
+++ Shared/SharedFoundation/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "SharedFoundation",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: "SharedFoundation", targets: ["SharedFoundation"])
+    ],
+    dependencies: [
+        .package(path: "../TestSupport")
+    ],
+    targets: [
+        .target(
+            name: "SharedFoundation",
+            swiftSettings: [.swiftLanguageMode(.v6)]
+        ),
+        .testTarget(
+            name: "SharedFoundationTests",
+            dependencies: [
+                "SharedFoundation",
+                .product(name: "TestSupport", package: "TestSupport")
+            ],
+            swiftSettings: [.swiftLanguageMode(.v6)]
+        )
+    ]
+)
