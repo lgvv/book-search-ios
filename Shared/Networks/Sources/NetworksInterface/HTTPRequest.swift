1a59a4fd27e018622f489177cecabe95be344189
이건우 <lgw9898@naver.com>
2024-10-10 22:35:00 +0900

    feat(networks): HTTPMethod, HTTPRequest, HTTPResponse 값 타입 정의

---
+++ Shared/Networks/Sources/NetworksInterface/HTTPRequest.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public struct HTTPRequest: Sendable {
+    public let method: HTTPMethod
+    public let url: URL
+    public let headers: [String: String]
+    public let body: Data?
+
+    public init(
+        method: HTTPMethod,
+        url: URL,
+        headers: [String: String] = [:],
+        body: Data? = nil
+    ) {
+        self.method = method
+        self.url = url
+        self.headers = headers
+        self.body = body
+    }
+
+    public init?(
+        method: HTTPMethod,
+        urlString: String,
+        headers: [String: String] = [:],
+        body: Data? = nil
+    ) {
+        guard let url = URL(string: urlString) else { return nil }
+        self.init(method: method, url: url, headers: headers, body: body)
+    }
+}
