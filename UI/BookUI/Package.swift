85a306fe5c56b4b449f46671849d4b72ec0bbb1d
이건우 <lgw9898@naver.com>
2024-10-30 22:11:00 +0900

    chore(spm): BookUI 패키지 스캐폴딩

---
+++ UI/BookUI/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "BookUI",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: "BookUI", targets: ["BookUI"])
+    ],
+    dependencies: [
+        .package(path: "../../Domain/BookDomain"),
+        .package(path: "../../Shared/ImageLoader"),
+        .package(path: "../DesignSystem")
+    ],
+    targets: [
+        .target(
+            name: "BookUI",
+            dependencies: [
+                .product(name: "BookModel", package: "BookDomain"),
+                .product(name: "ImageUI", package: "ImageLoader"),
+                .product(name: "DesignSystem", package: "DesignSystem")
+            ],
+            swiftSettings: [.swiftLanguageMode(.v6)]
+        )
+    ]
+)
