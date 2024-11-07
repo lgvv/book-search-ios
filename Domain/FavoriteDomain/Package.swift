378ef3b2555e21f3f7e9c9019b172819547b2ded
이건우 <lgw9898@naver.com>
2024-11-07 22:05:00 +0900

    chore(spm): FavoriteDomain에 FavoriteData 타깃 추가

--- Domain/FavoriteDomain/Package.swift
+++ Domain/FavoriteDomain/Package.swift
@@ -7,12 +7,15 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .library(name: "FavoriteCore", targets: ["FavoriteCore"])
+        .library(name: "FavoriteCore", targets: ["FavoriteCore"]),
+        .library(name: "FavoriteData", targets: ["FavoriteData"])
     ],
     dependencies: [
         .package(path: "../../Domain/BookDomain"),
         .package(path: "../../Shared/DependencyResolver"),
         .package(path: "../../Shared/SharedFoundation"),
+        .package(path: "../../Shared/Networks"),
+        .package(path: "../../Shared/TestSupport"),
     ],
     targets: [
         .target(
@@ -21,6 +24,15 @@ let package = Package(
                 .product(name: "BookModel", package: "BookDomain"),
                 .product(name: "DependencyResolver", package: "DependencyResolver"),
                 .product(name: "SharedFoundation", package: "SharedFoundation")
+            ],
+            swiftSettings: [.swiftLanguageMode(.v6)]
+        ),
+        .target(
+            name: "FavoriteData",
+            dependencies: [
+                .product(name: "NetworksInterface", package: "Networks"),
+                "FavoriteCore",
+                .product(name: "BookModel", package: "BookDomain"),
             ],
             swiftSettings: [.swiftLanguageMode(.v6)]
         )
