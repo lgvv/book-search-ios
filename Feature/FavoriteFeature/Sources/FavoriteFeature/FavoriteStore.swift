e0d3a3efc6fb295af0c4cd4ae9e623346da9354f
이건우 <lgw9898@naver.com>
2024-11-14 22:17:00 +0900

    feat(favorite): Effect를 실행하고 상태를 발행하는 FavoriteStore 구현

---
+++ Feature/FavoriteFeature/Sources/FavoriteFeature/FavoriteStore.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+import BookModel
+import DependencyResolver
+import FavoriteCore
+import FavoriteFeatureInterface
+import MemoCore
+import FeatureSupport
+
+@MainActor
+final class FavoriteStore {
+    var onDelegate: ((FavoriteDelegateAction) -> Void)?
+
+    private(set) var state = FavoriteReducer.State() {
+        didSet { subscriptions.notify(from: oldValue, to: state) }
+    }
+
+    private let subscriptions = StateSubscriptions<FavoriteReducer.State>()
+
+    func subscribe<Value: Equatable>(
+        _ scope: @escaping (FavoriteReducer.State) -> Value,
+        render: @escaping (_ old: Value?, _ new: Value) -> Void
+    ) {
+        subscriptions.add(scope: scope, current: state, render: render)
+    }
+
+    private let reducer = FavoriteReducer()
+    private let tasks = TaskScope<FavoriteReducer.CancelID>()
+    private let nonCancellables = NonCancellableTaskQueue<FavoriteReducer.CancelID>()
+    private let queue = ActionQueue<FavoriteReducer.Action>()
+
+    @Resolved(FavoriteClientKey.self) private var favoriteClient
+    @Resolved(MemoClientKey.self) private var memoClient
+
+    func send(_ action: FavoriteReducer.Action.ViewAction) {
+        dispatch(.view(action))
+    }
+
+    private func dispatch(_ action: FavoriteReducer.Action) {
+        queue.send(action) { action in
+            reducer.reduce(into: &state, action: action).forEach(handle)
+        }
+    }
+
+    private func handle(_ effect: FavoriteReducer.Effect) {
+        switch effect {
+        case let .delegate(action):
+            onDelegate?(action)
+
+        case let .cancel(id):
+            tasks.cancel(id)
+
+        case .observeFavorites:
+            tasks.run(.observeFavorites) { [weak self, favoriteClient] in
+                for await favorites in favoriteClient.observe().values {
+                    guard let self else { return }
+                    dispatch(.feedback(.favoritesChanged(favorites)))
+                }
+            }
+
+        case .reloadFavorites:
+            nonCancellables.enqueue(.reloadFavorites) { [favoriteClient] in
+                await favoriteClient.reload()
+            }
+
+        case let .removeFavorite(isbn):
+            nonCancellables.enqueue(.removeFavorite) { [favoriteClient] in
+                await favoriteClient.submitRemove(isbn)
+            }
+
+        case .observeMemos:
+            tasks.run(.observeMemos) { [weak self, memoClient] in
+                for await memoState in memoClient.observe().values {
+                    guard let self else { return }
+                    guard let memos = memoState.value else { continue }
+                    dispatch(.feedback(.memosChanged(Set(memos.map(\.book.isbn)))))
+                }
+            }
+        }
+    }
+}
