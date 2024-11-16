54b4388e1e4ab03978414b5085d26a78abec7917
이건우 <lgw9898@naver.com>
2024-11-16 10:42:00 +0900

    feat(memo): 메모 목록 상태와 리듀서 구현

---
+++ Feature/MemoFeature/Sources/MemoFeature/MemoList/MemoReducer.swift
@@ -0,0 +1,99 @@
+import Foundation
+
+import BookModel
+import MemoCore
+import MemoModel
+import MemoFeatureInterface
+import SharedFoundation
+
+struct MemoReducer: Sendable {
+
+    struct State: Sendable, Equatable {
+        var memos: ResourceState<[BookMemo]> = .loading
+        var favoriteISBNs = Set<String>()
+    }
+
+    enum Action: Sendable {
+        case view(ViewAction)
+        case feedback(FeedbackAction)
+
+        enum ViewAction: Sendable {
+            case viewDidLoad
+            case retryLoad
+            case toggleFavorite(Book)
+            case selectBook(Book)
+        }
+
+        enum FeedbackAction: Sendable {
+            case memosChanged(ResourceState<[BookMemo]>)
+            case favoritesChanged(Set<String>)
+        }
+    }
+
+    enum CancelID: Hashable, Sendable {
+        case observeMemos
+        case observeFavorites
+        case setFavorite
+        case reloadMemos
+    }
+
+    enum Effect: Sendable, Equatable {
+        case delegate(MemoDelegateAction)
+        case observeMemos
+        case reloadMemos
+        case observeFavorites
+        case setFavorite(Book, to: Bool)
+    }
+
+    func reduce(into state: inout State, action: Action) -> [Effect] {
+        switch action {
+        case let .view(action):
+            return reduceView(into: &state, action: action)
+        case let .feedback(action):
+            return reduceFeedback(into: &state, action: action)
+        }
+    }
+
+    private func reduceView(into state: inout State, action: Action.ViewAction) -> [Effect] {
+        switch action {
+        case .viewDidLoad:
+            return [
+                .observeMemos,
+                .observeFavorites
+            ]
+
+        case .retryLoad:
+            return [
+                .reloadMemos
+            ]
+
+        case let .toggleFavorite(book):
+            let willBeFavorite = !state.favoriteISBNs.contains(book.isbn)
+            if willBeFavorite {
+                state.favoriteISBNs.insert(book.isbn)
+            } else {
+                state.favoriteISBNs.remove(book.isbn)
+            }
+            return [
+                .setFavorite(book, to: willBeFavorite)
+            ]
+
+        case let .selectBook(book):
+            return [
+                .delegate(.didSelectBook(book))
+            ]
+        }
+    }
+
+    private func reduceFeedback(into state: inout State, action: Action.FeedbackAction) -> [Effect] {
+        switch action {
+        case let .memosChanged(memos):
+            state.memos = memos
+            return []
+
+        case let .favoritesChanged(isbns):
+            state.favoriteISBNs = isbns
+            return []
+        }
+    }
+}
