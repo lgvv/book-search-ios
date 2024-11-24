0e5e308a2b93ed36c8856eb670f4bf192821b83b
이건우 <lgw9898@naver.com>
2024-11-24 09:48:00 +0900

    feat(app): AppTab에 메모 탭 추가

--- App/BookSearch/Navigation/AppTab.swift
+++ App/BookSearch/Navigation/AppTab.swift
@@ -3,11 +3,13 @@ import UIKit
 enum AppTab: Int, CaseIterable {
     case search
     case favorite
+    case memo
 
     var title: String {
         switch self {
         case .search: "검색"
         case .favorite: "즐겨찾기"
+        case .memo: "메모"
         }
     }
 
@@ -15,6 +17,7 @@ enum AppTab: Int, CaseIterable {
         switch self {
         case .search: "magnifyingglass"
         case .favorite: "heart"
+        case .memo: "note.text"
         }
     }
 }
