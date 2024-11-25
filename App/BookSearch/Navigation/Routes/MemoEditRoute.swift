f7942a6a2612d7bcdcf3511e131e36bc1e3f20ab
이건우 <lgw9898@naver.com>
2024-11-25 22:11:00 +0900

    feat(app): BookDetailRoute와 MemoEditRoute 추가

---
+++ App/BookSearch/Navigation/Routes/MemoEditRoute.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+import BookModel
+
+struct MemoEditRoute: Route {
+    let book: Book
+
+    var presentation: RoutePresentation { .push }
+
+    func makeViewController(context: RouteContext) async -> UIViewController? {
+        let navigator = context.navigator
+        return context.scenes.makeMemoEditScene(book: self.book) { [weak navigator] in
+            navigator?.pop()
+        }
+    }
+}
